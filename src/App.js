b80608d8065ebb8adacb7066a152642c2a9cdd80
Sneha <sneha.adlakha@gmail.com>
2020-12-08 11:14:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😃": "Grinning Face with Big Eyes",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😆": "Grinning Squinting Face",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face"
+};
+var emojiList = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiEventHandler() {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    if (meaning === undefined) {
+      meaning = "We donot have this in our database";
+    }
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji){
+    var meaning = emojiDictionary[emoji];
+     setMeaning(meaning);
+
+  }
+  return (
+    <div className="App">
+      <h1>Input Emoji</h1>
+      <input onChange={emojiEventHandler}></input>
+      <h3>{meaning}</h3>
+      <h3>enojis we know</h3>
+      {
+        emojiList.map(function(emoji)
+        {
+          return <span
+          onClick = {()=> emojiClickHandler(emoji)}
+          style={{fontSize:"1rem" ,padding: "0.5rem",cursor: "pointer"}}key={emoji}>{emoji}</span>
+        })
+      }
+
+    </div>
+  );
+}
